45cd76f54f45d9c78c3a8d558a503d235b73844b
Jonathan Lee <lamproae@163.com>
2016-12-02 14:58:51 +0000

    Add file output

--- src/llog.c
+++ src/llog.c
@@ -2,11 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "llog.h"
 
 #define LLOG_DATE_LEN  32
 #define LLOG_MAX_LOG_LEN 1024
+#define LLOG_MAX_LOG_OUTPUT_LEN 2048
+
+#define LOCAL_LOG_FILE "log.txt"
 
 static char* color[LEVEL_MAX] = {
     "\033[1;37;40m", 
@@ -37,9 +45,11 @@ static char* getcurrenttime(char *buf, int size) {
 void llog(enum llog_level level, const char *fmt, ...) {
     char time[LLOG_DATE_LEN];
     char message[LLOG_MAX_LOG_LEN];
+    char message_output[LLOG_MAX_LOG_OUTPUT_LEN];
     va_list vars;
+    FILE* file;
 
-    if (level < DEBUG || level > FATAL || fmt == NULL) {
+   if (level < DEBUG || level > FATAL || fmt == NULL) {
         printf("%s[%s][%s]: %s%s\n", color[2], getcurrenttime(time, LLOG_MAX_LOG_LEN), level_str[2], "Invalid Input parameters for llog.", " \033[0m");
         return;
     }
@@ -47,6 +57,14 @@ void llog(enum llog_level level, const char *fmt, ...) {
     va_start(vars, fmt);
     vsnprintf(message, LLOG_MAX_LOG_LEN, fmt, vars);
     va_end(vars);
-    printf("%s[%s][%s]: %s%s\n", color[level], level_str[level], getcurrenttime(time, LLOG_DATE_LEN), message, " \033[0m");
+
+    snprintf(message_output, LLOG_MAX_LOG_OUTPUT_LEN, "%s[%s][%s]: %s%s\n", color[level], level_str[level], getcurrenttime(time, LLOG_DATE_LEN), message, " \033[0m");
+    fprintf(stdout, message_output, strlen(message_output));
+
+    file = fopen(LOCAL_LOG_FILE, "a+");
+    if (file != NULL) {
+        fprintf(file, message_output, strlen(message_output));
+        fclose(file);
+   }
 }
 
