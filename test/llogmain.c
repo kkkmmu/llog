0acca11cab99014e30aa60a1c63f7b2591cdb901
Jonathan Lee <lamproae@163.com>
2016-12-13 11:45:43 +0000

    Add debug macro

--- test/llogmain.c
+++ test/llogmain.c
@@ -17,6 +17,7 @@
  */
 #include <stdlib.h>
 #include "llog.h"
+#define NDBUG
 
 int main(int argc, char **argv) {
 
@@ -25,6 +26,15 @@ int main(int argc, char **argv) {
     llog(WARN, "%s", "Hello llog!"); 
     llog(ERROR, "%s", "Hello llog!"); 
     llog(FATAL, "%s", "Hello llog!"); 
+    debug("%s----%d-----%f\n", "liwei", 10, 100.111);
+    log_err("Welcome to: %s\n", "Beijing");
+    log_warn("Go to: %s\n", "Hell");
+    log_info("Stupid: %s\n", "apple");
+    check(1, "%s", "Success\n");
+    check(0, "%s", "Failure\n");
+    check_mem(NULL);
+    check_debug(NULL, "%s\n", "Debug NULL");
+error:
     return 0;
 }
 
